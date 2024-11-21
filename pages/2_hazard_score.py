d8e1acd9f315e3b43eae6c40cdf325bff806ee4d
AuHoh <a.hohmann45@gmail.com>
2024-11-21 09:28:56 +0100

    - add data for hazard map and adapt hazard map

--- pages/2_hazard_score.py
+++ pages/2_hazard_score.py
@@ -19,14 +19,14 @@ commune = gpd.read_parquet(com_sologne)
 
 
 # Obtenir les limites de la distribution
-bbox = commune.bounds.iloc[0]
+bbox = com_sologne.bounds.iloc[0]
 
 # Coordonnées du centre des limites
 center_latitude = (bbox['miny'] + bbox['maxy']) / 2
 center_longitude = (bbox['minx'] + bbox['maxx']) / 2
 
 # Créer une carte centrée sur les limites
-m = folium.Map(location=[center_latitude, center_longitude], zoom_start=8)
+m = folium.Map(location=[center_latitude, center_longitude], tiles="GeoportailFrance.orthos", zoom_start=8)
 
 popup = folium.GeoJsonPopup(fields=["NOM"], aliases=["Commune name"])
 
